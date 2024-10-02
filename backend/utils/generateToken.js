b8d40271f1ccbdbcd3f8eabe8f170c9037f6024b
darth-Vader1729 <divine.mathematician1729@gmail.com>
2024-10-02 11:53:56 +0530

    8) node env

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -10,7 +10,7 @@ const generateTokenAndSetCookies = (userId, res) => {
         maxAge : 15*24*60*60*1000,
         httpOnly : true, // prevent XSS(cross-site scripting) attacks 
         sameSite : "strict", // CSRF attacks cross-site request forgery attacks
-        secure : process.env.NODE_ENV !== "development"
+        // secure : process.env.NODE_ENV !== "development"
     })
 }
 
